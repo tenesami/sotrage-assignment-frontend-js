dc12bf8630bfcbc05ab165acf81336e63863a1e3
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-11-30 20:22:34 -0700

    add evetlistner to sotre the file insede buttom

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-fetch("http://localhost:3000/stores").then(function(res) {
-        return res.json()
-    })
-    .then(function(stores) {
-        console.log(stores)
-        stores.forEach(function(list) {
-            const new_list = document.createElement('h3')
-            new_list.innerText = list.item
-            document.body.appendChild(new_list)
+const display = document.getElementById("display")
+const sotre_container = document.getElementById("container")
+display.addEventListener("click", () => {
 
-            const new_discr = document.createElement('p')
-            new_discr.innerText = list.description
-            document.body.appendChild(new_discr)
+    fetch("http://localhost:3000/stores").then(function(res) {
+            return res.json()
+        })
+        .then(function(stores) {
+            console.log(stores)
+            stores.forEach(function(list) {
+                const new_list = document.createElement('h3')
+                new_list.innerText = list.item
+                sotre_container.appendChild(new_list)
+
+                const new_discr = document.createElement('p')
+                new_discr.innerText = list.description
+                sotre_container.appendChild(new_discr)
+            });
 
         });
-
-    });
+    //clear out the button after one click
+    sotre_container.innerHTML = ''
+})
